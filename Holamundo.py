45ea37063458b789c4ec26ecab61191b3f67b69d
kevinSolorzano <andreksg0101@gmail.com>
2023-09-08 13:46:39 -0500

    ProgramacionAplicada

---
+++ Holamundo.py
@@ -0,0 +1,12 @@
+print('Hola') 
+print("Hola Mundo") #comando para imprimir por pantalla 
+
+
+# Define the release and runtime integer variables below:
+release_year = 1977
+runtime = 1980
+
+# Este codigo imprime el tipo de dato que contiene la variable: 
+rating_out_of_10 = 6.8
+print(type(runtime)) 
+print(type(rating_out_of_10))
---
+++ quintoPrograma.py
@@ -0,0 +1,24 @@
+""" imprime la multiplicación del precio de un cafe por el numero de cafes
+    y aparte imprime el precio de un cafe y el numero de cafes."""
+
+precio_cafe = 1.50
+number_of_coffees = 4
+ 
+# Prints "6.0"
+precio_total=(precio_cafe * number_of_coffees)
+print("Precio total =")
+print(precio_total)
+# Prints "1.5"
+print(precio_cafe)
+# Prints "4"
+print(number_of_coffees)
+ 
+# Updating the price 
+precio_cafe = 2.00
+ 
+# Prints "8.0"
+print(precio_cafe * number_of_coffees)
+# Prints "2.0"
+print(precio_cafe)
+# Prints "4"
+print(number_of_coffees)
